4fb0d922d7b883c03829d6d61d954bc5b5f4616f
viktoriya.pa <viktoriya.pa@onyx.com>
2020-10-04 21:05:09 +0300

    start

--- src/main/java/com/pavlova/homeworks/lecture08/Genre.java
+++ src/main/java/com/pavlova/homeworks/lecture08/Genre.java
@@ -1,5 +1,19 @@
 package com.pavlova.homeworks.lecture08;
 
 public enum Genre {
-    FAIRY_TALE
+    FAIRY_TALE("fairy_tale"),
+    COMEDY("comedy"),
+    DOCUMENTARY("documentary"),
+    HORROR("horror"),
+    ADVENTURE("adventure"),
+    THRILLER("thriller"),
+    DETECTIVE("detective");
+
+    Genre(String genreLowerCase) {
+        this.genreLowerCase = genreLowerCase;
+    }
+
+    String genreLowerCase;
+
+
 }
---
+++ src/main/java/com/pavlova/homeworks/lecture08/Main.java
@@ -0,0 +1,7 @@
+package com.pavlova.homeworks.lecture08;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Genre.ADVENTURE);
+    }
+}
