1c3223f8503bacc87d04821ba63a48480ac4b3c5
viktoriya.pa <viktoriya.pa@onyx.com>
2020-10-21 22:46:18 +0300

    start

---
+++ src/main/java/com/pavlova/homeworks/lecture13/Mouse.java
@@ -0,0 +1,10 @@
+package com.pavlova.homeworks.lecture13;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
+public class Mouse {
+    private String name;
+    private int calories;
+}
